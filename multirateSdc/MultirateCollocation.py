ee4a68839e8f6c63578f1b3437c0fe4788f5b22c
Daniel Ruprecht <d.ruprecht@leeds.ac.uk>
2016-06-02 16:22:50 +0100

    adds multirateCollocation class and first test for integrate_p_pp1 function

---
+++ multirateSdc/MultirateCollocation.py
@@ -0,0 +1,47 @@
+from Collocation import *
+from CollocationClasses import *
+
+class multirateCollocation(object):
+
+  def __init__(self, M, P, tleft, tright):
+    # approximations of integrals from t_m to t_mp1
+    self.I_m_mp1 = np.zeros((M,1))
+    # approximations of integrals from t_p to t_pp1
+    self.I_p_pp1 = np.zeros((M,P))
+    assert tleft<tright, "tleft must be smaller than tright"
+    self.dt      = abs(tright - tleft)
+    self.M       = M
+    self.P       = P
+
+    self.coll    = CollGaussRadau_Right(num_nodes = M, tleft = tleft, tright = tright)
+    self.coll_sub = []
+    self.coll_sub.append(CollGaussRadau_Right(num_nodes=P, tleft = tleft, tright = self.coll.nodes[0]))
+    for i in range(1,M):
+      self.coll_sub.append(CollGaussRadau_Right(num_nodes=P, tleft = self.coll.nodes[i-1] , tright=self.coll.nodes[i]))
+    # NOTE: this assumes that the last node if self.coll is equal to tright; otherwise need one additional sub-step collocation class
+
+    self.S_mnp = np.zeros((M,M,P))
+    for n in range(M):
+      fl = np.zeros(M)
+      fl[n] = 1.0
+      coeff = self.coll._poly_newton(fl)
+      for m in range(M):
+        for p in range(P):
+          if p==0:
+            t_m_p = self.coll_sub[m].tleft
+          else:
+            t_m_p = self.coll_sub[m].nodes[p-1]
+          t_m_pp1 = self.coll_sub[m].nodes[p]
+          [nodes, weights] = CollBase._GaussLegendre(M, t_m_p, t_m_pp1)
+          flag    = self.coll._evaluate_horner(nodes, coeff)
+          self.S_mnp[m,n,p] = CollBase.evaluate(weights, flag)
+
+  def integrate_p_pp1(self, fu, m, p):
+    try:
+      fu = np.reshape(fu, (self.M,1))
+    except:
+      raise TypeError("Failed to convert argument fu into shape Mx1")
+    intvalue = 0.0
+    for n in range(self.M):
+      intvalue += self.S_mnp[m,n,p]*fu[n]
+    return intvalue
---
+++ multirateSdc/test_multirateCollocation.py
@@ -0,0 +1,30 @@
+from MultirateCollocation import multirateCollocation
+import unittest
+import numpy as np
+
+class test_problem(unittest.TestCase):
+
+  def setUp(self):
+    self.M = np.random.randint(8)+2
+    self.P = np.random.randint(3)+2
+    [self.tleft, self.tright] = np.sort(np.random.rand(2))
+
+  def test_caninstantiate(self):
+    mc_coll = multirateCollocation(self.M, self.P, self.tleft, self.tright)
+
+  def test_integrate_p_p1_linear(self):
+    mc_coll = multirateCollocation(self.M, self.P, self.tleft, self.tright)
+    slope = np.random.rand(1)
+    intercept = np.random.rand(1)
+    fu      = slope*mc_coll.coll.nodes + intercept
+    for m in range(self.M):
+      for p in range(self.P):
+        if p==0:
+          ta = mc_coll.coll_sub[m].tleft
+        else:
+          ta = mc_coll.coll_sub[m].nodes[p-1]
+        tb   = mc_coll.coll_sub[m].nodes[p]
+        intval  = mc_coll.integrate_p_pp1(fu, m, p) 
+        intex   = 0.5*slope*(tb**2-ta**2) + intercept*(tb - ta)
+        err     =  abs(intval - intex)
+        assert err<1e-14, ("Function integrate_p_pp1 failed to integrate linear function excatly. Error: %5.3e" % err)
