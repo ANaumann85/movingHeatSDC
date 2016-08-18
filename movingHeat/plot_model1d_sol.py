fad236e05d6a88deaf9be87cf21957e0613ecace
Daniel Ruprecht <d.ruprecht@leeds.ac.uk>
2016-08-18 14:44:51 +0200

    add simple script to plot scalar 1d model problem

---
+++ movingHeat/plot_model1d_sol.py
@@ -0,0 +1,49 @@
+import numpy as np
+import copy
+
+from matplotlib import pyplot as plt
+from pylab import rcParams
+from matplotlib.ticker import ScalarFormatter
+from subprocess import call
+
+nu    = 0.5
+k     = 1.0
+omega = 1.0
+y_0   = 1.0
+v_0   = 2.0
+
+def f(y):
+  return -nu*k*k*y
+
+def solve_f(y, dt):
+  return y/(1.0 + dt*nu*k*k)
+
+def g(t):
+  return np.exp(1j*omega*t)*(y_0 - v_0)/np.sqrt(2*np.pi)
+
+tend   = 100.0
+nsteps = 200
+taxis  = np.linspace(0,tend,nsteps+1)
+dt     = tend/float(nsteps)
+
+c          = y_0 - (y_0 - v_0)/( np.sqrt(2*np.pi)*(k*k*nu + 1j*omega) )
+taxis_plot = np.linspace(0,tend,1e5)
+y_exact    = c*np.exp(-nu*k*k*taxis_plot) + (y_0 - v_0)*np.exp(1j*taxis_plot*omega)/( np.sqrt(2*np.pi)*(k*k*nu + 1j*omega))
+
+y_euler = np.zeros(nsteps+1, dtype='complex')
+y_euler[0] = y_0
+for n in range(nsteps):
+  t            = float(n)*dt
+  y_temp       = y_euler[n] + dt*g(t)
+  y_euler[n+1] = solve_f(y_temp, dt)
+
+  # explicit Euler works correctly
+  #y_euler[n+1] = y_euler[n] + dt*(f(y_euler[n]) + g(t))
+
+print ("Error: %5.3e" % abs(y_euler[-1] - y_exact[-1]))
+fig = plt.figure()
+fig.clear()
+plt.plot(taxis_plot, y_exact.real, 'b')
+plt.plot(taxis, y_euler.real, 'r')
+plt.ylim([-1.0, 2.0])
+plt.show()
--- movingHeat/plot_model_sol.py
+++ movingHeat/plot_model_sol.py
@@ -8,13 +8,14 @@ from subprocess import call
 
 xaxis_u = np.linspace(-10, 0, 100)
 xaxis_v = np.linspace(0, 10, 100)
-dt     = 0.2
-nsteps = 40
-a      = 5.0
+dt     = 0.1
+nsteps = 80
+a      = 0.2
 kappa  = [0, 1, 2]
-ak0    = np.array([1.0, 0.1, 0.05])
-bk0    = np.array([0.0, 0.0, 0.0])
-ck0    = np.array([0.25, 0.2, 0.1])
+ak0    = np.array([1.0,    0.1, 0.00])
+ck0    = np.array([ak0[0], 2.0, 0.0])
+b10    = np.sum(ck0 - ak0)*a
+bk0    = np.array([0.0, b10, 0.0])
 dk0    = -bk0
 
 fig = plt.figure()
@@ -25,12 +26,10 @@ for n in range(nsteps):
   v = 0.0*xaxis_v
   for k in kappa:
     ak = ak0[k]*np.exp(-float(k)**2*t)
+    bk = bk0[k]*np.exp(-float(k)**2*t)
     ck = ck0[k]*np.exp(-float(k)**2*t)
-    if k==0.0:
-      bk = 0.0
-    else:
-      bk = (a/float(k))*(ak0[k] - ck0[k])*(np.exp(-float(k)**2*t)-1.0) + bk0[k]
-    dk = -bk
+    dk = dk0[k]*np.exp(-float(k)**2*t)
+    
     u  += ak*np.cos(k*xaxis_u) + bk*np.sin(k*xaxis_u)
     v  += ck*np.cos(k*xaxis_v) + dk*np.sin(k*xaxis_v)
   fig.clear()
@@ -39,4 +38,4 @@ for n in range(nsteps):
   plt.ylim([-1.0, 2.0])
   plt.title(("t = %3.2f" % t))
   plt.show(block=False)
-  plt.pause(0.02)
+  plt.pause(0.04)
