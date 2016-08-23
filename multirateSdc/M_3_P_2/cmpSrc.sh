a07c86ab9102769937bfbf063269e7282c5abfce
Andreas Naumann <andreas@NDHC41.math.tu-dresden.de>
2016-08-23 09:37:53 +0200

    konvergenz test, langes problem

--- multirateSdc/M_3_P_2/cmpSrc.sh
+++ multirateSdc/M_3_P_2/cmpSrc.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
-K="2 3 4 8" # 32"
-nStep="2 4 8 16"
+K="2 3 4 8 32 64"
+nStep="2 4 8 16 32 64 128"
 
 baseN="T_sdc_K_%d_%d_fixed_%d.vtu"
 ref="../T_imex_2000_fixed_2000.vtu"
--- multirateSdc/testDrake.sh
+++ multirateSdc/testDrake.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-K="2 3 4 8"
+K="2 3 4 8 32 64"
 #nstep="2 4 8"
-nstep="2 4 8 16"
+nstep="2 4 8 16 32 64 128"
 
 for k in $K; do
   for n in $nstep; do
