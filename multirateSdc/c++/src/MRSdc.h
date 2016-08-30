0cf1820b4ca0481176109eacd3c1fe5c72e3d0c3
ANaumann85 <a@b>
2016-08-30 17:03:18 +0200

    removed mtl

--- multirateSdc/c++/src/MRSdc.h
+++ multirateSdc/c++/src/MRSdc.h
@@ -2,14 +2,13 @@
 #define MRSDC_H
 
 #include <array>
-#include <boost/numeric/mtl/mtl.hpp>
 #include <iostream>
 #include <string>
 
 template<unsigned M >
 struct Collocation
 {
-	typedef mtl::dense2D<double > Mat;
+	typedef std::array<std::array<double, M+1>, M+1> Mat;
 	std::array<double, M> delta_m, nodes;
 	Mat sMat;
 	double tleft;
@@ -18,7 +17,7 @@ struct Collocation
 template< unsigned M, unsigned P >
 struct MultirateCollocation
 {
-	typedef mtl::dense2D<double > Mat;
+	typedef std::array<std::array<double, M>, M> Mat;
 
 	Mat Shat_mp;
 	std::array<Mat, M> S_mnp;
@@ -28,25 +27,25 @@ struct MultirateCollocation
 
 	MultirateCollocation()
 	{
-		Shat_mp = {{ 0.33333333 , 0.        },
-			{ 0.66666667,  0.        }};
+		Shat_mp[0] = { 0.33333333 , 0.};
+		Shat_mp[1] = { 0.66666667,  0.};
 
-		S_mnp[0]={{ 0.22916667,  0.1875    },
-			{-0.0625  ,   -0.02083333}};
-		S_mnp[1]={{ 0.25   ,     0.08333333},
-			{ 0.08333333 , 0.25      }};
+		S_mnp[0][0]={ 0.22916667,  0.1875    };
+		S_mnp[0][1]={-0.0625  ,   -0.02083333};
+		S_mnp[1][0]={ 0.25   ,     0.08333333};
+		S_mnp[1][1]={ 0.08333333 , 0.25      };
 	
-		coll.sMat={{ 0.   ,       0. ,         0.        },
-			{ 0.    ,      0.41666667, -0.08333333},
-			{ 0.   ,       0.33333333,  0.33333333}};
+		coll.sMat[0]={ 0. , 0. ,         0.        }; 
+		coll.sMat[1]={ 0. , 0.41666667, -0.08333333};
+		coll.sMat[2]={ 0. , 0.33333333,  0.33333333};
 
-		coll_sub[0].sMat={{ 0.   ,       0.     ,     0.        },
-			{ 0.  ,        0.25   ,    -0.08333333},
-			{ 0.  ,        0.08333333 , 0.08333333}};
+		coll_sub[0].sMat[0]={ 0.  , 0.  ,  0.        };
+		coll_sub[0].sMat[1]={ 0.  , 0.25, -0.08333333};
+		coll_sub[0].sMat[2]={ 0.  , 0.08333333 , 0.08333333};
 
-		coll_sub[1].sMat= {{ 0.    ,      0.    ,      0.        },
-			{ 0.  ,        0.5   ,     -0.16666667},
-			{ 0.   ,       0.16666667 , 0.16666667}};
+		coll_sub[1].sMat[0]={ 0. , 0.    ,      0.        };
+		coll_sub[1].sMat[1]={ 0. , 0.5   ,     -0.16666667};
+		coll_sub[1].sMat[2]={ 0. , 0.16666667 , 0.16666667};
 
 		coll.delta_m={0.33333333 , 0.66666667};
 		coll_sub[0].delta_m={0.16666667 , 0.16666667};
@@ -59,7 +58,7 @@ struct MultirateCollocation
 	{
 		setValue(res, 0.0);
 		for(unsigned j(0); j < M; ++j) {
-			axpy(coll.sMat(m+1,j+1), fu[j], res);
+			axpy(coll.sMat[m+1][j+1], fu[j], res);
 		}
 	}
 
@@ -68,7 +67,7 @@ struct MultirateCollocation
 	{
 		setValue(iVal, 0.0);
 		for(unsigned p(0); p < P; ++p)
-			axpy(Shat_mp(m,p), fu_sub[p], iVal);
+			axpy(Shat_mp[m][p], fu_sub[p], iVal);
 	}
 
 	template<typename Vec >
@@ -76,7 +75,7 @@ struct MultirateCollocation
 	{
 		setValue(iVal, 0.0);
 		for(unsigned n(0); n < M; ++n) {
-			axpy(S_mnp[m](n,p) ,fu[n], iVal);
+			axpy(S_mnp[m][n][p] ,fu[n], iVal);
 		}
 
 	}
@@ -85,9 +84,9 @@ struct MultirateCollocation
 	void integrate_p_pp1_sub(const std::array<Vec, P>& fu_sub , unsigned m, unsigned p, Vec& iVal)
 	{
 		setValue(iVal, 0.0);
-		const Mat& sMat(coll_sub[m].sMat);
+		const typename Collocation<M>::Mat& sMat(coll_sub[m].sMat);
 		for(unsigned j(0); j < P; ++j) {
-			axpy( sMat(p+1, j+1), fu_sub[j], iVal);
+			axpy( sMat[p+1][j+1], fu_sub[j], iVal);
 		}
 	}
 
