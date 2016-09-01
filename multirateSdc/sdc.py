37a83885e82dc7009e71059cf8b096a35c4aecb1
andreas <andreas@localhost>
2016-09-01 08:53:03 +0200

    switch sdc and multirate collocation back to last working test

--- multirateSdc/sdc.py
+++ multirateSdc/sdc.py
@@ -16,29 +16,25 @@ class sdc_step():
 
   '''
   '''
-  def update_I_m_mp1(self, u, usub):
+  def update_I_m_mp1(self, fu, fu_sub):
     try:
-      u     = np.reshape(u, (self.coll.M,self.prob.dim))
-      usub  = np.reshape(usub, (self.coll.M, self.coll.P, self.prob.dim))
+      fu     = np.reshape(fu, (self.coll.M,self.prob.dim))
+      fu_sub  = np.reshape(fu_sub, (self.coll.M, self.coll.P, self.prob.dim))
     except:
       raise
-    
-    fu, fu_sub = self.evaluate_f(u, usub)
     
     for m in range(self.coll.M):
       self.I_m_mp1[m,:] = self.coll.integrate_m_mp1(fu, m) + self.coll.integrate_m_mp1_sub(fu_sub[m,:,:], m)
 
   '''
   '''
-  def update_I_p_pp1(self, u, usub):
+  def update_I_p_pp1(self, fu, fu_sub):
     try:
-      u     = np.reshape(u, (self.coll.M,self.prob.dim))
-      usub  = np.reshape(usub, (self.coll.M, self.coll.P, self.prob.dim))
+      fu     = np.reshape(fu, (self.coll.M,self.prob.dim))
+      fu_sub  = np.reshape(fu_sub, (self.coll.M, self.coll.P, self.prob.dim))
     except:
       raise
     
-    fu, fu_sub = self.evaluate_f(u, usub)
-  
     for m in range(self.coll.M):
       for p in range(self.coll.P):
         self.I_p_pp1[m,p,:] = self.coll.integrate_p_pp1( fu, m, p ) + self.coll.integrate_p_pp1_sub( fu_sub[m,:,:], m, p )
@@ -108,11 +104,13 @@ class sdc_step():
 
   '''
   '''
-  def predict(self, u0, u, usub):
+  def predict(self, u0, u, usub, fu, fu_sub):
     try:
-      u    = np.reshape(u, (self.coll.M, self.prob.dim))
-      usub = np.reshape(usub, (self.coll.M, self.coll.P, self.prob.dim))
-      u0   = np.reshape(u0, (self.prob.dim,))
+      u      = np.reshape(u, (self.coll.M, self.prob.dim))
+      usub   = np.reshape(usub, (self.coll.M, self.coll.P, self.prob.dim))
+      fu     = np.reshape(fu, (self.coll.M, self.prob.dim))
+      fu_sub = np.reshape(fu_sub, (self.coll.M, self.coll.P, self.prob.dim))
+      u0     = np.reshape(u0, (self.prob.dim,))
     except:
       raise
 
@@ -122,55 +120,79 @@ class sdc_step():
       
         # standard step
         u[m,:] = self.prob.solve_f1(self.coll.coll.delta_m[m], u0_step)
+        
         # embedded steps
         t = self.coll.coll_sub[m].tleft
         usub[m,0,:] = u0_step + self.coll.coll_sub[m].delta_m[0]*( self.prob.f1(u[m,:]) + self.prob.f2(u0_step, t) )
+        fu_sub[m,0,:] = self.prob.f2(usub[m,0,:], self.coll.coll_sub[m].nodes[0])
+        
         for p in range(1,self.coll.P):
           t = self.coll.coll_sub[m].nodes[p-1]
           usub[m,p,:] = usub[m,p-1,:] + self.coll.coll_sub[m].delta_m[p]*( self.prob.f1(u[m,:]) + self.prob.f2(usub[m,p-1,:], t) )
-
+          fu_sub[m,p,:] = self.prob.f2(usub[m,p,:], self.coll.coll_sub[m].nodes[p])
+        
         # overwrite standard value
         u0_step = usub[m,self.coll.P-1,:]
         u[m,:]  = usub[m,self.coll.P-1,:]
+        fu[m,:] = self.prob.f1(u[m,:])
 
-    return u, usub
+    return 0
 
 
   '''
   '''
-  def sweep(self, u0, u, usub, u_, usub_):
+  def sweep(self, u0, u, usub, fu, fu_sub):
     try:
+    
       u     = np.reshape(u, (self.coll.M,self.prob.dim))
-      u_    = np.reshape(u_, (self.coll.M,self.prob.dim))
       usub  = np.reshape(usub, (self.coll.M, self.coll.P, self.prob.dim))
-      usub_ = np.reshape(usub_, (self.coll.M, self.coll.P, self.prob.dim))
-
+      fu    = np.reshape(fu, (self.coll.M,self.prob.dim))
+      fu_sub  = np.reshape(fu_sub, (self.coll.M, self.coll.P, self.prob.dim))
+    
     except:
       raise
     
     # update integral terms
-    self.update_I_m_mp1(u_, usub_)
-    self.update_I_p_pp1(u_, usub_)
+    self.update_I_m_mp1(fu, fu_sub)
+    self.update_I_p_pp1(fu, fu_sub)
 
-    u0_step = u0
-
+    u_mm1 = u0
+    
     for m in range(self.coll.M):
       
       # standard step
-      rhs  = u0_step - self.coll.coll.delta_m[m]*( self.prob.f1(u_[m,:]) ) + self.I_m_mp1[m,:]
-      u[m,:] = self.prob.solve_f1(self.coll.coll.delta_m[m], rhs)
+      rhs     = u_mm1 - self.coll.coll.delta_m[m]*( fu[m,:] ) + self.I_m_mp1[m,:]
+      fu_star = self.prob.f1(self.prob.solve_f1(self.coll.coll.delta_m[m], rhs))
       
-      # embedded steps
-      t = self.coll.coll_sub[m].tleft
-      usub[m,0,:] = u0_step + self.coll.coll_sub[m].delta_m[0]*( self.prob.f1(u[m,:]) - self.prob.f1(u_[m,:]) + self.prob.f2(u0_step, t) - self.prob.f2(u0_step, t) ) + self.I_p_pp1[m,0,:]
-      for p in range(1,self.coll.P):
-        t = self.coll.coll_sub[m].nodes[p-1]
-        usub[m,p,:] = usub[m,p-1,:] + self.coll.coll_sub[m].delta_m[p]*( self.prob.f1(u[m,:]) - self.prob.f1(u_[m,:]) + self.prob.f2(usub[m,p-1,:], t) - self.prob.f2(usub_[m,p-1,:], t) ) + self.I_p_pp1[m,p,:]
-
-      # overwrite standard value
-      u0_step = usub[m,self.coll.P-1,:]
+      # --- embedded steps ---
+      for p in range(self.coll.P):
+        
+        if p==0:
+          t = self.coll.coll_sub[m].tleft
+          f2_term = 0.0
+          # Define initial value for embedded step
+          if m==0:
+            usub_mm1 = u0
+          else:
+            usub_mm1 = u[m-1,:]
+          # --------
+        else:
+          t            = self.coll.coll_sub[m].nodes[p-1]
+          f2_term      = fu_sub[m,p-1,:] - fu_pm1_old
+          usub_mm1     = usub[m,p-1,:]
+        
+        usub[m,p,:]  = usub_mm1 + self.coll.coll_sub[m].delta_m[p]*( fu_star - fu[m,:] + f2_term) + self.I_p_pp1[m,p,:]
+        
+        # save value in fu_sub[m,p,:] for next iteration before overwriting it
+        fu_pm1_old    = fu_sub[m,p,:]
+        fu_sub[m,p,:] = self.prob.f2(usub[m,p,:], self.coll.coll_sub[m].nodes[p])
+      # --- end of embedded steps ---
+      
+      # Prepare for next standard time step
       u[m,:]  = usub[m,self.coll.P-1,:]
-
+      u_mm1   = u[m,:]
+      fu[m,:] = self.prob.f1(u[m,:])
+      
     return 0
 
   '''
@@ -193,4 +215,4 @@ class sdc_step():
     except:
       raise
     assert self.prob.lambda_1==0.0, "Can only compute embedded collocation solution analytically if lambda_1=0"
-    return np.linalg.inv(Mcoll).dot(u0*np.ones(self.coll.P))
+    return np.reshape( np.linalg.inv(Mcoll).dot(u0*np.ones(self.coll.P)), (self.coll.P, 1))
