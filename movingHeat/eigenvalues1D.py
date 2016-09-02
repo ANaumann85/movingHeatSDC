a4d09c955ee14187b56d5cef08b45134d33bca26
andreas <andreas@localhost>
2016-09-02 08:25:14 +0200

    the eigenvalues are time independent  in 1d

--- movingHeat/eigenvalues1D.py
+++ movingHeat/eigenvalues1D.py
@@ -8,10 +8,16 @@ import matplotlib.pyplot as plt
 
 #periodic 1d heat with source al\delta(x-at)(v0-u(t,x)), i.e. 
 #\dot{u} = \Delta u + al\delta(x-at)(v0-u(t,x))
-#stencil:
+#approximate \delta(x-at) as point or as triangle with unit area
+#stencil point:
 #\dot u_{0}=(u_{N}-2u_0+u_1)/h^2 +al\delta(x_0-at)(v_0-u_0)
 #\dot u_{j}=(u_{j-1}-2u_{j}+u_{j+1})/h^2+al\delta(x_j-at)(v_0-u_j), j=N-1..1
 #\dot u_{N}=(u_{N-1}-2u_N+u_1)/h^2+al\delta(x_N-at)(v_0-u_N)
+#stencil unit area:
+#\dot u_{0}=(u_{N}-2u_0+u_1)/h^2 +al*tri(x_0-at,h)(v_0-u_0)/h
+#\dot u_{j}=(u_{j-1}-2u_{j}+u_{j+1})/h^2+al*tri(x_j-at,h)(v_0-u_j)/h, j=N-1..1
+#\dot u_{N}=(u_{N-1}-2u_N+u_1)/h^2+al*tri(x_N-at,h)(v_0-u_N)/h
+#utilizing tri(x,h)=1-|x/h| 0<=x<h, 0 else
 
 alpha=5e-4
 nu=1e-3
@@ -28,19 +34,23 @@ A[0,N-1]=1
 A[N-1,0]=1
 A=A*nu
 
+eigLapl, v=dLA.eig(A)
 dt=te/nT
 allEigs=[]
 for k in range(nT):
     t=k*dt
     jc=int(a*t/h)
-    B=A
-    B[jc,jc] +=-alpha
+    B=np.copy(A)
+    B[jc,jc] +=-alpha/h
     l,v=dLA.eig(B)
     allEigs.append(np.real(np.sort(l)))
 
+diffEigs=np.zeros(nT)
 for k in range(1,nT):
-    print max(abs(allEigs[0]-allEigs[k]))
+    diffEigs[k-1]=max(abs(allEigs[0]-allEigs[k]))
+print "maximal absolute difference of eigenvalues at t and t=0", diffEigs
 #print A
+plt.plot(np.sort(eigLapl), '--', label='lapl')
 for k in range(nT):
     plt.plot(allEigs[k], label='t=%0.1f' % (k*dt))
 #plt.legend(loc='lower center', bbox_to_anchor=(0.5,-0.2), ncol=6)
