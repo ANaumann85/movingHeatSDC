4bdb0efd59a523420fce5b74567c2d8ab06adde0
ANaumann85 <a@b>
2016-09-19 10:44:51 +0200

    eigenvalues of the 2d problem

--- movingHeat/movingHeat/eigenvalues/alpha_1_nu1/getEigs.py
+++ movingHeat/movingHeat/eigenvalues/alpha_1_nu1/getEigs.py
@@ -35,3 +35,26 @@ ax1.set_position([box.x0, box.y0 + box.height * 0.1,
         box.width, box.height * 0.9])
 plt.legend(loc='upper center', bbox_to_anchor=(0.5,-0.05), fancybox=True, shadow=True, ncol=3)
 plt.savefig('eigenvalues.pdf',bbox_inches='tight')
+
+fig=plt.figure(figsize=(8,6))
+for k in range(1, nT):
+    plt.plot(abs(np.divide(allEigLmv[0]-allEigLmv[k],allEigLmv[0])), label='t=%d' %(k*dt))
+plt.legend(loc='upper center', bbox_to_anchor=(0.5,-0.05), fancybox=True, shadow=True, ncol=4)
+plt.savefig('eigenvalues_reldiff0.pdf',bbox_inches='tight')
+
+fig=plt.figure(figsize=(8,6))
+for k in range(1, nT):
+    plt.plot(abs(allEigLmv[0]-allEigLmv[k]), label='t=%d' %(k*dt))
+plt.legend(loc='upper center', bbox_to_anchor=(0.5,-0.05), fancybox=True, shadow=True, ncol=4)
+plt.savefig('eigenvalues_absdiff0.pdf',bbox_inches='tight')
+
+fig=plt.figure(figsize=(8,6))
+plt.plot(abs(allEigLmv[0]-allEigLmv[1]), label='t=%d' %(1*dt))
+plt.legend(loc='upper center', bbox_to_anchor=(0.5,-0.05), fancybox=True, shadow=True, ncol=4)
+plt.savefig('eigenvalues_absdiff_0-3.pdf',bbox_inches='tight')
+
+fig=plt.figure(figsize=(8,6))
+plt.plot((allEigLmv[0]-allEigLmv[1]), label='t=%d' %(1*dt))
+plt.legend(loc='upper center', bbox_to_anchor=(0.5,-0.05), fancybox=True, shadow=True, ncol=4)
+plt.savefig('eigenvalues_diff_0-3.pdf',bbox_inches='tight')
+print np.real(allEigLmv[0][len(allEigLmv[0])-10:len(allEigLmv[0])])
