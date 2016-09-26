cc36bf07f2606e4ac7852ee748c636d0af3cf51c
andreas <andreas@localhost>
2016-09-26 08:07:12 +0200

    stability of ros2 looks ways better now

--- movingHeat/movingHeat/src/stability_ros2.cc
+++ movingHeat/movingHeat/src/stability_ros2.cc
@@ -12,99 +12,100 @@ namespace Dune
 
 std::vector<double > linspace(double v0, double ve, unsigned nValues)
 {
-	if(nValues < 2)
-		nValues == 2;
-	double dv=(ve-v0)/(nValues-1);
-	std::vector<double > ret(nValues);
-	ret[0]=v0;
-	for(unsigned i(1); i < nValues; ++i)
-		ret[i] = v0+i*dv;
-	return ret;
+  if(nValues < 2)
+    nValues == 2;
+  double dv=(ve-v0)/(nValues-1);
+  std::vector<double > ret(nValues);
+  ret[0]=v0;
+  for(unsigned i(1); i < nValues; ++i)
+    ret[i] = v0+i*dv;
+  return ret;
 }
 
 struct ColMat
 {
-	typedef std::vector<double > V;
-	V data;
-	unsigned nr, nc;
-	ColMat(unsigned nR, unsigned nC):
-		data(nR*nC),
-		nr(nR), nc(nC)
-	{}
+  typedef std::vector<double > V;
+  V data;
+  unsigned nr, nc;
+  ColMat(unsigned nR, unsigned nC):
+    data(nR*nC),
+    nr(nR), nc(nC)
+  {}
 
-	template<typename VIn >
-	void setColumn(unsigned c, const VIn& v)
-	{
-		unsigned s(c*nr);
-		for(unsigned i(0); i < nr; ++i, ++s) {
-			data[s] = v[i];
-		}
-	}
+  template<typename VIn >
+    void setColumn(unsigned c, const VIn& v)
+    {
+      unsigned s(c*nr);
+      for(unsigned i(0); i < nr; ++i, ++s) {
+        data[s] = v[i];
+      }
+    }
 
-	inline const double& operator()(unsigned r, unsigned c) const
-	{ return data[c*nr+r]; }
+  inline const double& operator()(unsigned r, unsigned c) const
+  { return data[c*nr+r]; }
 
-	inline double& operator()(unsigned r, unsigned c)
-	{ return data[c*nr+r]; }
+  inline double& operator()(unsigned r, unsigned c)
+  { return data[c*nr+r]; }
 
-	double* getData() 
-	{ return data.data(); }
+  double* getData() 
+  { return data.data(); }
 };
 
 std::ostream& operator<<(std::ostream& f, const ColMat& mat)
 {
-	f.precision(15);
-	for(unsigned r(0); r < mat.nr; ++r) {
-			f<< mat(r,0);
-		for(unsigned c(1); c < mat.nc; ++c)
-			f<< " " << mat(r,c);
-		f << std::endl;
-	}
+  f.precision(15);
+  for(unsigned r(0); r < mat.nr; ++r) {
+    f<< mat(r,0);
+    for(unsigned c(1); c < mat.nc; ++c)
+      f<< " " << mat(r,c);
+    f << std::endl;
+  }
   return f;
 }
 
 extern "C" {
-	void dgeev_(const char* JOBVL, const char* JOBVR, const int* N,
-                       const double* A, const int* LDA, double* WR,  double* WI,
-                       double* VL, const int* LDVL, double* VR, const int* LDVR,
-                       double* WORK, const int* LWORK, int* INFO);
+  void dgeev_(const char* JOBVL, const char* JOBVR, const int* N,
+      const double* A, const int* LDA, double* WR,  double* WI,
+      double* VL, const int* LDVL, double* VR, const int* LDVR,
+      double* WORK, const int* LWORK, int* INFO);
 }
+
 double getMaxAbsEig(ColMat& cm)
 {
-	char job('N');
-	int N(cm.nr), one(1), lwork(-1), info(0);	
-	std::vector<double > lr(N), li(N), work(1);
+  char job('N');
+  int N(cm.nr), one(1), lwork(-1), info(0);	
+  std::vector<double > lr(N), li(N), work(1);
 
-	double vl;
-	
+  double vl;
+
 #if 0
-	{std::fstream mat("mat_in.mtx", std::ios_base::out | std::ios_base::trunc);
-	mat << cm;
-	mat.close();}
+  {std::fstream mat("mat_in.mtx", std::ios_base::out | std::ios_base::trunc);
+    mat << cm;
+    mat.close();}
 #endif
-	//request memory
-	dgeev_(&job, &job, &N, cm.getData(), &N, lr.data(), li.data(), &vl, &one, &vl, &one, work.data(), 
-			&lwork, &info);
+  //request memory
+  dgeev_(&job, &job, &N, cm.getData(), &N, lr.data(), li.data(), &vl, &one, &vl, &one, work.data(), 
+      &lwork, &info);
 
-	lwork = (int) work[0];
-	work.resize(lwork);
-	dgeev_(&job, &job, &N, cm.getData(), &N, lr.data(), li.data(), &vl, &one, &vl, &one, work.data(), 
-			&lwork, &info);
+  lwork = (int) work[0];
+  work.resize(lwork);
+  dgeev_(&job, &job, &N, cm.getData(), &N, lr.data(), li.data(), &vl, &one, &vl, &one, work.data(), 
+      &lwork, &info);
 
 #if 1
-	{std::fstream eigs("eigs.mtx",  std::ios_base::out | std::ios_base::trunc);
-		eigs.precision(15);
-	//for(auto& d:lr) eigs << d << std::endl;}
-	for(unsigned i(0); i < N; ++i) eigs << std::complex<double>(lr[i], li[i]) << std::endl;}
+  {std::fstream eigs("eigs.mtx",  std::ios_base::out | std::ios_base::trunc);
+    eigs.precision(15);
+    //for(auto& d:lr) eigs << d << std::endl;}
+    for(unsigned i(0); i < N; ++i) eigs << std::complex<double>(lr[i], li[i]) << std::endl;}
 #endif
 
-	vl = lr[0]*lr[0]+li[0]*li[0];
-	for(unsigned i(1); i < N; ++i) {
-		double cur = lr[i]*lr[i]+li[i]*li[i];
-		if( cur > vl)
-			vl=cur;
-	}
-	return sqrt(vl);
+    vl = lr[0]*lr[0]+li[0]*li[0];
+    for(unsigned i(1); i < N; ++i) {
+      double cur = lr[i]*lr[i]+li[i]*li[i];
+      if( cur > vl)
+        vl=cur;
+    }
+return sqrt(vl);
 }
 
 int main(int argc, char* argv[])
@@ -122,8 +123,8 @@ int main(int argc, char* argv[])
   heat.init(y0); y0 = 0.0;
 
   unsigned nTests(20);
-  auto alpha_vec=linspace(0.0, 4.0, nTests);
-  auto nu_vec=linspace(0.0, 5.0, nTests);
+  auto alpha_vec=linspace(0.0, 15.0, nTests);
+  auto nu_vec=linspace(0.0, 2, nTests); //nTests);
   ColMat colMat(y0.size(), y0.size());
   ColMat maxEigs(alpha_vec.size()+1, nu_vec.size()+1);
 #if 1
