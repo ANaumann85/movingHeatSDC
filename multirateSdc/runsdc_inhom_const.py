6d422fbaa6fe870b80cdb4cf331269501e9e0d7f
ANaumann85 <a@b>
2016-11-08 19:42:09 +0100

    example for constant inhomogenous source and mrsdc

--- multirateSdc/runsdc_inhom_const.py
+++ multirateSdc/runsdc_inhom_const.py
@@ -7,7 +7,8 @@ import copy
 
 nu = -1.0
 b=2.0
-prob = problem_inh(nu, b)
+prob = problem_inh(nu, b, True)
+probConstFast = problem_inh(nu, b, True)
 M = 3
 P = 8
 tstart = 0.0
@@ -19,16 +20,21 @@ u0 = -b/nu
 dt=(tend-tstart)/nIter
 for nK in nKVec:
     u0_=u0
+    us0_=u0
     res=[]
     for n in range(nIter):
       ts=tstart+n*dt
       te=ts+dt
       sdc = sdc_step(M, P, ts, te, prob)
-      sdc_stan=sdc_standard_step(M, ts, te, prob)
+      sdc_stan=sdc_standard_step(M, ts, te, probConstFast)
       u = np.zeros((M,prob.dim))
       usub = np.zeros((M,P,prob.dim))
       fu = np.zeros((M,prob.dim))
       fu_sub = np.zeros((M,P,prob.dim))
+
+      us      = np.zeros((M,1,1))
+      us_     = np.zeros((M,1,1))
+      fsu     = np.zeros((M, 1))
 
     # run predictor
       sdc.predict(u0_, u, usub, fu, fu_sub)
@@ -38,20 +44,17 @@ for nK in nKVec:
         u_old = copy.deepcopy(u)
         usub_old = copy.deepcopy(usub)
         sdc.sweep(u0_, u, usub, fu, fu_sub)
+        print sdc.residual(u0_, u)
       u0_ = u[-1]
-      res.append(u0_[0])
+
+      sdc_stan.predict(us0_, us_)
+      for k in range(nK):
+        sdc_stan.sweep(us0_, us, us_)
+        us_    = copy.deepcopy(us)
+
     uex = u0*np.exp(nu*tend) + b/nu*(np.exp(nu*tend)-1)
     err =  abs(uex - u[-1])
     print "mrsdc:", err,uex, u[-1]
 
-    us0_=u0
-    u      = np.zeros((M,1,1))
-    u_     = np.zeros((M,1,1))
-    fu     = np.zeros((M, 1))
-
-    sdc_stan.predict(us0_, u_)
-    for k in range(nK):
-        sdc_stan.sweep(us0_, u, u_)
-        u_    = copy.deepcopy(u)
-    err =  abs(uex - u[-1])
-    print "imex:",err,uex, u[-1]
+    err =  abs(uex - us[-1])
+    print "imex:",err.flatten(),uex, us[-1]
