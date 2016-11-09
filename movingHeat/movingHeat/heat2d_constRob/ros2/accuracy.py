e526a3a383eec6624e9644cb15c74d8f039fd319
ANaumann85 <a@b>
2016-11-09 13:32:15 +0100

    efffect of predictor with constant source

---
+++ movingHeat/movingHeat/heat2d_constRob/ros2/accuracy.py
@@ -0,0 +1,21 @@
+from diff import getDiff
+from math import log
+from tail import *
+
+te=20.0
+nTest=13
+nStepsRef=5*(2**nTest)
+refFile="heat_ros2_%d-00001.vtu" % nStepsRef
+
+print "#h l2 max nstep runtime[ms]"
+for n in range(0, nTest):
+    nstep=(5*(2**n))
+    h=te/nstep
+    dFile1="heat_ros2_%d-00001.vtu" % nstep
+    res=getDiff(refFile, dFile1)
+    log="log_"+str(nstep)
+    logf=open(log)
+    lastLine=tail(logf, 1)
+    logf.close()
+    print h,res[0],res[1],nstep,lastLine[0].split(':')[1].split()[0]
+
