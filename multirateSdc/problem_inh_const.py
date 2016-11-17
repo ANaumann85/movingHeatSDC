f92f4ae72b4d620f0b3e8b708c4def3f831149a5
andreas <andreas@localhost>
2016-11-17 14:26:07 +0100

    inhom const mit getrennten nu

--- multirateSdc/problem_inh_const.py
+++ multirateSdc/problem_inh_const.py
@@ -2,19 +2,22 @@ import numpy as np
 
 class problem_inh_const():
 
-  def __init__(self, nu, b):
-    self.nu   = nu
+  def __init__(self, nu1, nu2, b):
+    self.nu1 = nu1
+    self.nu2 = nu2
+    #self.nu1=nu1+nu2
+    #self.nu2=0
     self.dim = 1
     self.b=b
     
   def f1(self, u):
-    return self.nu*u
+    return self.nu1*u
 
   def solve_f1(self, a, b):
-    return b/(1.0 - self.nu*a)
+    return b/(1.0 - self.nu1*a)
 
   def constSrc(self):
     return self.b
 
   def f2(self, u_, t):
-    return self.b
+    return self.nu2*u_+self.b
--- multirateSdc/runsdc_inhom_const.py
+++ multirateSdc/runsdc_inhom_const.py
@@ -5,18 +5,24 @@ import numpy as np
 import unittest
 import copy
 
-nu = -4.0
-b=2.0
-prob = problem_inh(nu, b)
-M = 3
+nu1 = -2.0
+nu2 = -0.1
+nu = nu1+nu2
+b=10.0
+prob = problem_inh(nu1, nu2, b)
+M = 2
 P = 8
 tstart = 0.0
 tend   = 1.0
-nIter=1
-nKVec=range(0,5)
-u0 = -b/nu
-
+nIter=2
+nKVec=range(0,10)
+nIterVec=[2**n for n in range(0,5)]
+u0 = 0.5 #-b/nu
+nK=4
 dt=(tend-tstart)/nIter
+
+#for nIter in nIterVec:
+#    dt=(tend-tstart)/nIter
 for nK in nKVec:
     u0_=u0
     us0_=u0
@@ -24,36 +30,38 @@ for nK in nKVec:
     for n in range(nIter):
       ts=tstart+n*dt
       te=ts+dt
-      sdc = sdc_step(M, P, ts, te, prob)
+      sdc = sdc_step(M, P, ts, te, prob,theta=1)
       sdc_stan=sdc_standard_step(M, ts, te, prob)
       u = np.zeros((M,prob.dim))
       usub = np.zeros((M,P,prob.dim))
       fu = np.zeros((M,prob.dim))
       fu_sub = np.zeros((M,P,prob.dim))
 
-      us      = np.zeros((M,1,1))
-      us_     = np.zeros((M,1,1))
-      fsu     = np.zeros((M, 1))
+      us      = np.zeros((M,1))
+      us_     = np.zeros((M,1))
 
     # run predictor
       sdc.predict(u0_, u, usub, fu, fu_sub)
 
       for k in range(nK):
         # run standard node sweep...
-        u_old = copy.deepcopy(u)
-        usub_old = copy.deepcopy(usub)
+        #u_old = copy.deepcopy(u)
+        #usub_old = copy.deepcopy(usub)
         sdc.sweep(u0_, u, usub, fu, fu_sub)
         #print sdc.residual(u0_, u)
-      u0_ = u[-1]
+      u0_ = copy.deepcopy(u[-1])
 
       sdc_stan.predict(us0_, us_)
+      us = copy.deepcopy(us_)
       for k in range(nK):
         sdc_stan.sweep(us0_, us, us_)
         us_    = copy.deepcopy(us)
+        #print "resid:",sdc_stan.residual(us0_, us)
+      us0_=us[-1]
 
     uex = u0*np.exp(nu*tend) + b/nu*(np.exp(nu*tend)-1)
     err =  abs(uex - u[-1])
     print "mrsdc:", err,uex, u[-1]
 
     err =  abs(uex - us[-1])
-    print "imex:",err.flatten(),uex, us[-1]
+    print "imex: ",err.flatten(),uex, us[-1]
