problem with time dependent coefficient does work - fixed wrong sign in exact solution

multirateSdc/problem_tcoeff.py
@@ -2,8 +2,9 @@ import numpy as np
 
 class problem_tcoeff():
 
-  def __init__(self, nu):
+  def __init__(self, nu, a):
     self.nu  = nu
+    self.a   = a
     self.dim = 1
     
   def f1(self, u):
@@ -13,4 +14,4 @@ class problem_tcoeff():
     return b/(1.0 - self.nu*a)
 
   def f2(self, u_, t):
-    return np.cos(t)*u_
+    return self.a*np.cos(t)*u_

multirateSdc/runsdc_tcoeff.py
@@ -6,20 +6,29 @@ import numpy as np
 import unittest
 import copy
 
-nu = -1.0
-prob = problem_tcoeff(nu)
-M = 5
-P = 5
-tstart = 0.0
-tend   = 0.5
-nIter=10
-u0 = 1.0
+from matplotlib import pyplot as plt
 
-dt=(tend-tstart)/nIter
+# Consider the IVP
+# y'(t) = cos(t)*y(t) - nu*y(t); y(0) = c
+# with solution
+# y(t) = c*exp[sin(t) - nu*t]
+
+nu     = -0.1
+a      = 1.0
+prob   = problem_tcoeff(nu, a)
+M      = 5
+P      = 5
+tstart = 0.0
+tend   = 10.0
+nsteps = 10
+u0     = 1.0
+
+dt=(tend-tstart)/float(nsteps)
 u0_=u0
 u0_std = u0
-res=[]
-for n in range(nIter):
+u_mrsdc=[u0]
+u_sdc  = [u0]
+for n in range(nsteps):
   ts=tstart+n*dt
   te=ts+dt
   sdc = sdc_step(M, P, ts, te, prob)
@@ -40,7 +49,7 @@ for n in range(nIter):
     usub_old = copy.deepcopy(usub)
     sdc.sweep(u0_, u, usub, fu, fu_sub)
   u0_ = u[-1]
-  res.append(u0_[0])
+  u_mrsdc.append(u0_)
 
   ### standard SDC ###
   u_std = np.zeros((M, prob.dim))
@@ -51,21 +60,19 @@ for n in range(nIter):
     u_old_std = copy.deepcopy(u_std)
     sdc_standard.sweep(u0_std, u_std, u_old_std)
   u0_std = copy.deepcopy(u_std[-1])
+  u_sdc.append(u0_std)
 
-uex = u0*(np.exp(np.sin(tend) - nu*tend))
+uex = u0*np.exp(a*np.sin(tend) + nu*tend)
 err =  abs(uex - u[-1])
-print err
-f=open("problem_inh.dat",'w')
-print res
-for k in res:
-  f.write(str(k)+"\n")
-f.close()
-print ""
-print "Standard SDC"
+print ("MR-SDC error: %5.3e" % err)
 print ""
 err =  abs(uex - u_std[-1])
-print err
-f=open("problem_inh_std.dat",'w')
-print res
-for k in res:
-  f.write(str(k)+"\n")
+print ("Standard SDC error: %5.3e" % err)
+
+taxis = np.linspace(0, tend, nsteps+1)
+uex_pl = u0*np.exp(a*np.sin(taxis) + nu*taxis)
+plt.figure(1)
+plt.plot(taxis, u_mrsdc, 'b')
+plt.plot(taxis, u_sdc, 'r')
+plt.plot(taxis, uex_pl, 'k')
+plt.show()